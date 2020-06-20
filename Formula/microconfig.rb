3d21ed0203b38efde336c22e63b3434d942104f6
Evgeni Sladkowski <kapodes@gmail.com>
2020-06-20 17:32:23 +0200

    version 4.1.4 bugfix

--- Formula/microconfig.rb
+++ Formula/microconfig.rb
@@ -6,11 +6,11 @@ class Microconfig < Formula
 
   if OS.mac?
     url "https://github.com/microconfig/microconfig/releases/download/v4.1.4/microconfig-macos.zip"
-    sha256 "2999368d8d399362302ca96511ecf716b232743e9dd44dd02b12a192824a47b7"
+    sha256 "088b18882b29f934c272f48ea734b28e6cb52c5b764b2ff616d9b4902cba8717"
   elsif OS.linux?
     if Hardware::CPU.intel?
       url "https://github.com/microconfig/microconfig/releases/download/v4.1.4/microconfig-linux.zip"
-      sha256 "9ed4d47bc62875eacfb833882124bce53eaeeb739d57473b6562f34119759525"
+      sha256 "65b8fc529b2784df4a1fb5d1befc6e50cb9391d60c3549f094abdf61a2c01876"
     end
   end
 
