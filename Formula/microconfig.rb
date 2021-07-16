b3934231a026f085fc92e542405ed7249a64ac35
Alex <Matorin.A.Aleks@sberbank.ru>
2021-07-16 18:34:19 +0300

    updated version 4.4.2

--- Formula/microconfig.rb
+++ Formula/microconfig.rb
@@ -1,16 +1,16 @@
 class Microconfig < Formula
   desc "Modern way of microservice configuration management"
   homepage "https://microconfig.io/"
-  version "4.4.2"
+  version "4.4.3"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/microconfig/microconfig/releases/download/v4.4.2/microconfig-macos.zip"
-    sha256 "b01dd0f64cea48daeeda69e37f7156e5bda24806f66cef71c126c536bf5d6e99"
+    url "https://github.com/microconfig/microconfig/releases/download/v4.4.3/microconfig-macos.zip"
+    sha256 "a570d23b0ce0e615ba97c55996cb9fc76da7578f0dd62e979f3e3ac2550dfbcd"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/microconfig/microconfig/releases/download/v4.4.2/microconfig-linux.zip"
-      sha256 "d146d5860ea960b385533122814acbc21c6ef74c0428ec141bf04aec91031391"
+      url "https://github.com/microconfig/microconfig/releases/download/v4.4.3/microconfig-linux.zip"
+      sha256 "f11c7f9b7a5d0b31765f22d79d12531c6334695821e33a04c46cd065c473d659"
     end
   end
 
