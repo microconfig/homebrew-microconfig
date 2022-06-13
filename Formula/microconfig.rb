9a114b46cddac6abed53c02a37b8037986a50677
Evgeni Sladkowski <kapodes@gmail.com>
2022-06-13 17:02:39 +0200

    Update microconfig.rb
    version 4.6.0 release

--- Formula/microconfig.rb
+++ Formula/microconfig.rb
@@ -1,15 +1,15 @@
 class Microconfig < Formula
   desc "Modern way of microservice configuration management"
   homepage "https://microconfig.io/"
-  version "4.5.0"
+  version "4.6.0"
 
   if OS.mac?
-    url "https://github.com/microconfig/microconfig/releases/download/v4.5.0/microconfig-macos.zip"
-    sha256 "ae3abf0093732d1b8593fac8f81e551f2758fb29c32613492e9d362fd6f8d069"
+    url "https://github.com/microconfig/microconfig/releases/download/v4.6.0/microconfig-macos.zip"
+    sha256 "a99a80638e4704691ff1f7f5131ef93bc7f5717a553ac19b1245791adf310f9b"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/microconfig/microconfig/releases/download/v4.5.0/microconfig-linux.zip"
-      sha256 "3ae00ca7eb8af720e2a9536c984bd52b97f46d8d6b9198dc4379e2ce8594f944"
+      url "https://github.com/microconfig/microconfig/releases/download/v4.6.0/microconfig-linux.zip"
+      sha256 "f37d211be8c7e91b2998fe523daa23dc887d0839a2c084656fe8fdbcf60c8744"
     end
   end
 
