c3a80494c9193ac1b447ae82d42e266853151a88
Evgeni Sladkowski <kapodes@gmail.com>
2024-01-05 11:50:02 +0100

    release 4.9.3

--- Formula/microconfig.rb
+++ Formula/microconfig.rb
@@ -1,15 +1,15 @@
 class Microconfig < Formula
   desc "Modern way of microservice configuration management"
   homepage "https://microconfig.io/"
-  version "4.9.0"
+  version "4.9.3"
 
   if OS.mac?
-    url "https://github.com/microconfig/microconfig/releases/download/v4.9.0/microconfig-macos.zip"
-    sha256 "302d77b5effd47dd9d4fb9abc485a0b9aa84d806529e3e647cdb4cd7a32e0a8c"
+    url "https://github.com/microconfig/microconfig/releases/download/v4.9.3/microconfig-macos.zip"
+    sha256 "dcafa3103a3a02789fdcf49a231637009eed0c86d6ae8543710977cf64a3546f"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/microconfig/microconfig/releases/download/v4.9.0/microconfig-linux.zip"
-      sha256 "6932db5c574d12db3636d2f75d82f425387943a991e1fedda3cb9f2bdd9d033f"
+      url "https://github.com/microconfig/microconfig/releases/download/v4.9.3/microconfig-linux.zip"
+      sha256 "79bf8f45e9d6b8a8e3a2611663829ae24f8ef698b7d2a9f5bebb423c35bd8d80"
     end
   end
 
